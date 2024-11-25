2623bb61d0faa916d5c6efe3d6203f45aca88edd
Connor Smith <connorhsmith@gmail.com>
2024-11-26 01:35:15 +1100

    wip

--- financials/main.py
+++ financials/main.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 API_ENDPOINT = "https://api.opencollective.com/graphql/v2"
@@ -14,7 +17,7 @@ query account($account: String) {
     account(slug: $account) {
         name
         slug
-        transactions(limit: 10, type: CREDIT) {
+        transactions(limit: 100, type: CREDIT) {
             totalCount
             nodes {
                 fromAccount {
@@ -32,14 +35,14 @@ query account($account: String) {
 
 payload = {"query": query, "variables": {"account": "twohoursonelife"}}
 headers = {"Content-Type": "application/json", "Api-key": OC_API_KEY}
-# response = requests.post(API_ENDPOINT, json=payload, headers=headers)
-# response.raise_for_status()
+response = requests.post(API_ENDPOINT, json=payload, headers=headers)
+response.raise_for_status()
 
-# contributions = pd.json_normalize(
-#     response.json()["data"]["account"]["transactions"]["nodes"]
-# )
+contributions = pd.json_normalize(
+    response.json()["data"]["account"]["transactions"]["nodes"]
+)
 
-# print(contributions)
+print(contributions)
 
 
 # Get last set of transactions? from sqlite db
@@ -51,7 +54,16 @@ headers = {"Content-Type": "application/json", "Api-key": OC_API_KEY}
 # Format transactions? to json
 
 # Call webhook with transactions?
-embed = {"title": "I am a title", "description": "I am a description"}
-json = {"content": "this is pretty cool", "embeds": [embed]}
+embed = {
+    "title": "@person donated $$$",
+    "color": 16759605,
+    "timestamp": "2018-08-17T07:00:00",
+}
+json = {
+    "username": "Open Collective",
+    "avatar_url": "https://cdn.discordapp.com/avatars/948569181513724024/dab2d4c4ae7f5b2253a97dde3ef09a67.webp?size=80",
+    "content": "[New contribution](https://linky.link) to 2HOL on [Open Collective](https://opencollective.com/twohoursonelife)!",
+    "embeds": [embed],
+}
 webhook_response = requests.post(WEBHOOK_URL, json=json)
 webhook_response.raise_for_status()
